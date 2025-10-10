0e6d5eac57640e44f4b935cf4d090ee07bea9c6f
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-10-09 20:46:20 -0500

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: 'AIzaSyASkjBK3F2DdCQkK2eG_5aJhM7GEcy101w',
+    authDomain: 'mytinderapp-1831a.firebaseapp.com',
+    projectId: 'mytinderapp-1831a',
+    storageBucket: 'mytinderapp-1831a.firebasestorage.app',
+    messagingSenderId: '61914774489',
+    appId: '1:61914774489:web:9e7253dbfa400df76ef7c8',
+  },
+
+  SUPABASE_CONFIG: {
+    SUPABASE_URL: 'https://imbsrrnbpaegopmprjyg.supabase.co',
+    SUPABASE_KEY:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltYnNycm5icGFlZ29wbXByanlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMzg1OTIsImV4cCI6MjA3NTYxNDU5Mn0.W-5BVPO0yeuZCEk4BgNEn2mtPrb5RXa8I8hcfmLew9o',
+    SUPABASE_BUCKET1: 'ProfileImages',
+    SUPABASE_BUCKET2: 'images',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,19 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  FIREBASE_CONFIG: {
+    apiKey: 'AIzaSyASkjBK3F2DdCQkK2eG_5aJhM7GEcy101w',
+    authDomain: 'mytinderapp-1831a.firebaseapp.com',
+    projectId: 'mytinderapp-1831a',
+    storageBucket: 'mytinderapp-1831a.firebasestorage.app',
+    messagingSenderId: '61914774489',
+    appId: '1:61914774489:web:9e7253dbfa400df76ef7c8',
+  },
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  SUPABASE_CONFIG: {
+    SUPABASE_URL: 'https://imbsrrnbpaegopmprjyg.supabase.co',
+    SUPABASE_KEY:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltYnNycm5icGFlZ29wbXByanlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMzg1OTIsImV4cCI6MjA3NTYxNDU5Mn0.W-5BVPO0yeuZCEk4BgNEn2mtPrb5RXa8I8hcfmLew9o',
+    SUPABASE_BUCKET1: 'ProfileImages',
+    SUPABASE_BUCKET2: 'images',
+  },
+};
