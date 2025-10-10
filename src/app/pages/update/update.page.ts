77b3e9ae1004655d4e19f6724d56039de17429e0
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-10-09 21:04:30 -0500

    Cree las paginas home, register, login y update y las declare con standalone=false

---
+++ src/app/pages/update/update.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update',
+  templateUrl: './update.page.html',
+  styleUrls: ['./update.page.scss'],
+  standalone: false,
+})
+export class UpdatePage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
